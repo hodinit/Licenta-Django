f8f51a36bb7f2c2cdb36721cca885985287439ee
HoratiuLazar <horatiulazar2003@gmail.com>
2025-05-04 17:42:27 +0300

    start of sprint 3. functional form added.

--- licenta/app/views.py
+++ licenta/app/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.core.serializers import serialize
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .models import Location
+from .forms import LocationForm
 import json
 
 def hello_world(request):
@@ -15,5 +17,16 @@ def location_list(request):
 def homepage(request):
     return render(request, 'homepage.html')
 
+@login_required
 def addspot(request):
-    return render(request, 'addspot.html')
+    if request.method == 'POST':
+        form = LocationForm(request.POST, request.FILES)
+        if form.is_valid():
+            spot = form.save(commit=False)
+            spot.added_by = request.user
+            spot.save()
+            return redirect('locations')
+    else:
+        form = LocationForm()
+    
+    return render(request, 'addspot.html', {'form': form})
