04cba5855a0ee1fba36999b0bb7e75ea1a70010b
HoratiuLazar <horatiulazar2003@gmail.com>
2025-06-28 00:50:21 +0300

    gg

--- licenta/app/admin.py
+++ licenta/app/admin.py
@@ -3,7 +3,7 @@ from .models import Location
 
 @admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
-    list_display = ('name', 'approved', 'available', 'added_by', 'latitude', 'longitude')
+    list_display = ('name', '_id', 'approved', 'available', 'added_by', 'latitude', 'longitude')
     search_fields = ('name', 'description')
     readonly_fields = ('created_at', 'updated_at', 'payment')
     list_editable = ('approved', 'available')
